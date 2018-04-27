3003e2e57ff12a7986944b5cb5c1714a57cc29bb
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2018-04-27 23:48:05 +0530

    Added version

---
+++ cvutils/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Package initialization.
+Created on Thu Apr 19 22:00:00 2018
+Author: Prasun Roy | CVPRU-ISICAL (http://www.isical.ac.in/~cvpr)
+GitHub: https://github.com/prasunroy/cvutils
+
+"""
+
+
+__version__ = '0.1.0'
